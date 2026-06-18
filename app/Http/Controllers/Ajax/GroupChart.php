b0f7de994b2e2d4ef37425b9e3df1a420236ef0d
Ivan Mykhavko <tegosiv@gmail.com>
2026-06-18 17:42:37 +0300

    chore: replace Input facade with Request in GroupChart (Input removed in L6)

--- app/Http/Controllers/Ajax/GroupChart.php
+++ app/Http/Controllers/Ajax/GroupChart.php
@@ -3,44 +3,43 @@
 namespace App\Http\Controllers\Ajax;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Input as Input;
+use Illuminate\Http\Request;
 
 class GroupChart extends Controller
 {
-	public function get()
-	{
-		$imageKeys = Input::get('imageKeys');
-		$featureDataOfImages = json_decode(Input::get('featureDataOfImages'), true);
+    public function get(Request $request)
+    {
+        $imageKeys = $request->input('imageKeys');
+        $featureDataOfImages = json_decode($request->input('featureDataOfImages'), true);
 
-		$imageKeysData = [];
-		foreach ($imageKeys as $key => $imageKey) {
-			$imageKeysData[] = $imageKey;
-		}
+        $imageKeysData = [];
+        foreach ($imageKeys as $key => $imageKey) {
+            $imageKeysData[] = $imageKey;
+        }
 
-		$resultFeatures = [];
-		$imageKeysDataIndex = [];
-		$min = [];
-		$max = [];
+        $resultFeatures = [];
+        $imageKeysDataIndex = [];
+        $min = [];
+        $max = [];
 
-		foreach ($imageKeysData as $imageKey) {
-			$resultFeatures[] = $featureDataOfImages[$imageKey];
-			$imageKeysDataIndex[] = $imageKey;
-			$min[] = min($featureDataOfImages[$imageKey]);
-			$max[] = max($featureDataOfImages[$imageKey]);
-		}
+        foreach ($imageKeysData as $imageKey) {
+            $resultFeatures[] = $featureDataOfImages[$imageKey];
+            $imageKeysDataIndex[] = $imageKey;
+            $min[] = min($featureDataOfImages[$imageKey]);
+            $max[] = max($featureDataOfImages[$imageKey]);
+        }
 
-		$seriesData = [];
+        $seriesData = [];
 
-		for ($i = 0; $i < count($resultFeatures); $i++) {
-			$seriesData[] = [
-				'data' => $resultFeatures[$i],
-				'name' => "Частина {$imageKeysDataIndex[$i]}",
-				'min' => min($min) - 15,
-				'max' => max($max) + 15
-			];
+        for ($i = 0; $i < count($resultFeatures); $i++) {
+            $seriesData[] = [
+                'data' => $resultFeatures[$i],
+                'name' => "Частина {$imageKeysDataIndex[$i]}",
+                'min' => min($min) - 15,
+                'max' => max($max) + 15
+            ];
+        }
 
-		}
-
-		return response()->json($seriesData);
-	}
+        return response()->json($seriesData);
+    }
 }
